a31df786cf2d79f244700c7f862951c52cbaf520
ViniciosG <viniciossantanag@gmail.com>
2021-03-27 21:04:26 -0300

    HTTP Status Code - Parte 2 - POST PUT DELETE

---
+++ modulo-08/carros/src/main/java/com/carros/api/CarrosController.java
@@ -0,0 +1,83 @@
+package com.carros.api;
+
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
+import com.carros.domain.dto.CarroDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/carros")
+public class CarrosController {
+    @Autowired
+    private CarroService service;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<CarroDTO> carros = service.getCarros();
+        return ResponseEntity.ok(carros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<CarroDTO> carro = service.getCarroById(id);
+
+        return carro
+                .map(c -> ResponseEntity.ok(c))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/tipo/{tipo}")
+    public ResponseEntity getCarrosByTipo(@PathVariable("tipo") String tipo) {
+        List<CarroDTO> carros = service.getCarrosByTipo(tipo);
+
+        return carros.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok(carros);
+    }
+    @PostMapping
+    public ResponseEntity post(@RequestBody Carro carro) {
+
+        try {
+            CarroDTO c = service.insert(carro);
+
+            URI location = getUri(c);
+            return ResponseEntity.created(location).build();
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+    private URI getUri(CarroDTO c) {
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(c.getId()).toUri();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity put(@PathVariable("id") Long id,@RequestBody Carro carro) {
+
+        CarroDTO c = service.update(carro, id);
+
+        return c != null ? 
+        		ResponseEntity.ok(c):
+        			ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+       boolean ok = service.delete(id);
+
+        return ok ?
+          		ResponseEntity.ok().build():
+        			ResponseEntity.notFound().build();
+    }
+}
---
+++ modulo-08/carros/src/main/java/com/carros/domain/Carro.java
@@ -0,0 +1,17 @@
+package com.carros.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    private String nome;
+    private String tipo;
+}
